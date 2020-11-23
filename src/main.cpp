ea94d647153912336cc71bef325e0f5d7dd1bd4e
Johannes Kühnel <johannes.kuehnel@fau.de>
2020-11-23 20:30:43 +0100

    blynk example

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
+
 #include <Arduino.h>
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "YourAuthToken";
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
 
 int analogPin = A0;
 float read_curr_ma = 0.0;
 float max_curr_ma = 20.0;
 float adc_intervals = 1023.0;
 
+BlynkTimer timer;
+
+void timer_event() {
+  read_curr_ma = analogRead(analogPin) * (max_curr_ma / adc_intervals); 
+  Serial.println(read_curr_ma);  
+  Blynk.virtualWrite(V5, read_curr_ma);
+}
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+  timer.setInterval(1000L, timer_event);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  read_curr_ma = analogRead(analogPin) * (max_curr_ma / adc_intervals); 
-  Serial.println(read_curr_ma);        
+  Blynk.run();
+  timer.run();
 }
